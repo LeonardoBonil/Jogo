2fafeaffe3ea1dae89f54bd189ed99be557d44fb
LeonardoBonil <83551200+LeonardoBonil@users.noreply.github.com>
2021-05-02 15:59:49 -0300

    ok
    ok

---
+++ Jonathan Jogo/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    public int vidaMaxima;
+    public int vidaAtual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaAtual = vidaMaxima;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReceberDano()
+{
+    vidaAtual = vidaAtual - 1; 
+    if(vidaAtual <=0)
+	{
+	  Debug.Log("Game Over");
+	}
+ }
+}
