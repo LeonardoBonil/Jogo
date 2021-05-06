74ece17667c82fe98c26bf9ec3281d56a576d0c5
LeonardoBonil <83551200+LeonardoBonil@users.noreply.github.com>
2021-05-06 15:06:22 -0300

    O K
    com os Menus

---
+++ JogoJonathan(1.0)/Assets/Scripts/Fruit.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public bool collected;
+    public int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collider){if(collider.gameObject.tag == "Player"){Destroy(gameObject); GameController.instance.totalScore += Score; collected = true;}}
+}
